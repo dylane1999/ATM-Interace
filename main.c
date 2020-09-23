16bd1fa8b54b31b8997b6217b1bb667ec91db63d
Dylan Edwards <dylanedwards290@gmail.com>
2020-09-23 16:30:40 -0700

    header file

---
+++ main.c
@@ -0,0 +1,14 @@
+int main(void){
+
+  char username[12];
+  char password[12];
+  printf("input username: ");
+  scanf("%s", username);
+  printf("\ninput password: ");
+  scanf("%s", password);
+  
+
+
+
+}
+
